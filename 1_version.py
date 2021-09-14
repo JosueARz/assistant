5d3cfbfcdadbdbfe18ff1da16ebfebea22ca221c
Josue AyRz <josueayalaruiz@gmail.com>
2021-09-14 15:32:34 -0500

    add speed oof voice

--- 1_version.py
+++ 1_version.py
@@ -6,6 +6,8 @@ frase = str(input('Escribir una frase aqui '))
 engine  = pyttsx3.init()
 voice = engine.getProperty('voices')
 engine.setProperty('voice', voice[3].id )
+newVoiceRate = 120  
+engine.setProperty('rate', newVoiceRate)
 
 def speak(audio):
     engine.say(audio)
