c4f8c0d609a42b1d586753f446e40c82cd8aaa7f
Josue AyRz <josueayalaruiz@gmail.com>
2021-09-14 15:48:14 -0500

    add welcome and time in real time

--- 1_version.py
+++ 1_version.py
@@ -1,8 +1,6 @@
 import pyttsx3
 import datetime
 
-frase = str(input('Escribir una frase aqui '))
-
 engine  = pyttsx3.init()
 voice = engine.getProperty('voices')
 engine.setProperty('voice', voice[3].id )
@@ -28,8 +26,21 @@ def date():
     day = int(datetime.datetime.now().day)
     speak('The day is')
     speak(day)
-    
 
-speak(frase)
-time()
-date()    
+def  welc():
+    speak('Welcome back sir!')
+    hour = datetime.datetime.now().hour
+
+    if hour >= 6 and hour <=12:
+        speak('Good morning')
+    else:
+        if hour >12 and hour <= 18:
+            speak('Good aftrnoon')
+        else:
+            if hour > 18 and hour <= 24:
+                speak('Good evening')
+            else:
+                speak('Good night')
+        
+    speak('How can i hel you')
+welc()
