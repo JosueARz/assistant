a740f96c4134ee94ab2de481f3b20562bece2046
Josue AyRz <josueayalaruiz@gmail.com>
2021-09-14 15:38:40 -0500

    add time and date

--- 1_version.py
+++ 1_version.py
@@ -13,4 +13,23 @@ def speak(audio):
     engine.say(audio)
     engine.runAndWait()
     
-speak(frase)  
+def time():
+    Time = datetime.datetime.now().strftime("%H:%M:%S")
+    speak('Its')
+    speak(Time)
+
+def date():
+    year = int(datetime.datetime.now().year)
+    speak('The year is')
+    speak(year)
+    month = int(datetime.datetime.now().month)
+    speak('The number of the month is')
+    speak(month)
+    day = int(datetime.datetime.now().day)
+    speak('The day is')
+    speak(day)
+    
+
+speak(frase)
+time()
+date()    
